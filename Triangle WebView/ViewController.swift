d95e2a2ae73f1e03e8744b23c482a23f3775d4f0
José Pablo Iglesias <JPIglesias-Triangle@users.noreply.github.com>
2018-08-26 01:53:22 -0500

    Initial Commit

---
+++ Triangle WebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Triangle WebView
+//
+//  Created by José Pablo Iglesias on 26/08/18.
+//  Copyright © 2018 Triangle México. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
